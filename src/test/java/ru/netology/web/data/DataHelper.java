7af634d82ef9edd8918dfa28ad0002218003b4c4
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-08-04 18:37:31 +0300

    DataHelper has modified

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -1,6 +1,9 @@
 package ru.netology.web.data;
 
 import lombok.Value;
+import org.checkerframework.checker.units.qual.C;
+
+import java.util.Random;
 
 public class DataHelper {
   private DataHelper() {}
@@ -26,5 +29,31 @@ public class DataHelper {
 
   public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
     return new VerificationCode("12345");
+  }
+
+  @Value
+  public static class CardNumber {
+    private String cardNumber;
+  }
+
+  public static CardNumber getFirstCardNumber() {
+    return new CardNumber("5559 0000 0000 0000");
+  }
+
+  public static CardNumber getSecondCardNumber() {
+    return new CardNumber("5559 0000 0000 0002");
+  }
+
+  @Value
+  public static class TransferAmount {
+    private String amount;
+  }
+
+  public static int generateValidAmount(int balance) {
+    return new Random().nextInt(balance) + 1;
+  }
+
+  public static int generateInvalidAmount(int balance) {
+    return Math.abs(balance) + new Random().nextInt(10000);
   }
 }
