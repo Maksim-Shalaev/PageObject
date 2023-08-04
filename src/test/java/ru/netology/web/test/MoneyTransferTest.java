56833c86b50a5cfc30ceff7e54c74a16f6a8d67f
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-08-04 19:06:32 +0300

    TransferPage has benn added and modified

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -2,9 +2,7 @@ package ru.netology.web.test;
 
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
-import ru.netology.web.page.LoginPageV1;
-import ru.netology.web.page.LoginPageV2;
-import ru.netology.web.page.LoginPageV3;
+import ru.netology.web.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -23,7 +21,7 @@ class MoneyTransferTest {
   @Test
   void shouldTransferMoneyBetweenOwnCardsV2() {
     open("http://localhost:9999");
-    var loginPage = new LoginPageV2();
+    var loginPage = new LoginPage();
 //    var loginPage = open("http://localhost:9999", LoginPageV2.class);
     var authInfo = DataHelper.getAuthInfo();
     var verificationPage = loginPage.validLogin(authInfo);
